27d64a056280778a31bb5f9c3d7f22446117742d
Thawã Borges <69798681+Thawaboeges@users.noreply.github.com>
2025-04-25 09:58:58 -0300

    Update Codigo_Def.py

--- Projeto_F/Codigo_Def.py
+++ Projeto_F/Codigo_Def.py
@@ -14,7 +14,7 @@ def extrair_numeros(url, termos_busca):
         headers = {
             'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers) # Acessa a URL fornecida
         response.raise_for_status()  # Lança um erro se a requisição falhar
         soup = BeautifulSoup(response.text, 'html.parser')
         texto = soup.get_text()
